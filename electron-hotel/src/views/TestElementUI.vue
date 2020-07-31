caa6784be7818597fdc3891dc46749059f5a8dca
MysticalGuest <1317148109@qq.com>
2020-07-31 11:22:30 +0800

    Create TestElementUI.vue

---
+++ electron-hotel/src/views/TestElementUI.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="gui">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'gui',
+  components: {
+    HelloWorld
+  }
+}
+</script>
