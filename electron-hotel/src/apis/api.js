717e1da23cf35a7528becbdf3344d46857dd766d
MysticalGuest <1317148109@qq.com>
2020-07-31 11:16:43 +0800

    Create api.js

---
+++ electron-hotel/src/apis/api.js
@@ -0,0 +1,13 @@
+/**
+ * api接口统一管理
+ */
+import { get, post } from '@/request/http'
+
+export default {
+  postData(action, params) {
+    return post(action, params)
+  },
+  getData(action, params) {
+    return get(action, params)
+  }
+}
