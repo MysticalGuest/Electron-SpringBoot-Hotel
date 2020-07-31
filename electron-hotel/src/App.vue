772afbae86d1ceb4997997fceb948dc1e1642a98
MysticalGuest <1317148109@qq.com>
2020-07-31 11:16:47 +0800

    Create App.vue

---
+++ electron-hotel/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Home from '@/views/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  margin-top 20px
+
+</style>
