4fe57279e41571f6aaba534abd7b160e283a6f72
MysticalGuest <1317148109@qq.com>
2020-07-31 11:24:08 +0800

    Create SpringbootConfig.java

---
+++ spring-server/src/main/java/com/hotel/configs/SpringbootConfig.java
@@ -0,0 +1,58 @@
+package com.hotel.configs;
+
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.hotel.interceptors.CheckLoginInterceptor;
+
+@Configuration
+public class SpringbootConfig extends WebMvcConfigurerAdapter {
+	@Autowired
+	private CheckLoginInterceptor checkLoginInterceptor;
+
+	// 注册拦截器
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		//前台所有界面拦截
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/front/Home");
+//		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/front/Bill");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/front/ApartmentManagement");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/front/AccountOfPerDay");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/front/Statistics");
+		
+		//管理员所有界面拦截
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/administrator/CustomerInfoForAdm");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/administrator/IntegratedManagement");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/administrator/ApartmentManageAdm");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/administrator/Account");
+		registry.addInterceptor(checkLoginInterceptor).addPathPatterns("/administrator/Statistics");
+	}
+	@Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+}
