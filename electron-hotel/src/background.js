d0ae50aa0c4b81531a1d7711a783400c0dc87d1a
MysticalGuest <1317148109@qq.com>
2020-07-31 11:43:55 +0800

    Update background.js

--- electron-hotel/src/background.js
+++ electron-hotel/src/background.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// import { app, protocol, BrowserWindow, Menu } from 'electron'
-import { app, protocol, BrowserWindow} from 'electron'
+import { app, protocol, BrowserWindow, Menu } from 'electron'
+// import { app, protocol, BrowserWindow} from 'electron'
 import {
   createProtocol
   // installVueDevtools
@@ -11,6 +11,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
+let serverProcess = null
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
@@ -50,28 +51,28 @@ function createWindow() {
   win.on('closed', () => {
     win = null
   })
-  // createMenu()
+  createMenu()
 }
 
 // 设置菜单栏
-// function createMenu() {
-//   // darwin表示macOS，针对macOS的设置
-//   if (process.platform === 'darwin') {
-//     const template = [{
-//       label: 'App Demo',
-//       submenu: [
-//         {role: 'about'},
-//         {
-//           role: 'quit'
-//         }]
-//     }]
-//     const menu = Menu.buildFromTemplate(template)
-//     Menu.setApplicationMenu(menu)
-//   } else {
-//     // windows及linux系统
-//     Menu.setApplicationMenu(null)
-//   }
-// }
+function createMenu() {
+  // darwin表示macOS，针对macOS的设置
+  if (process.platform === 'darwin') {
+    const template = [{
+      label: 'App Demo',
+      submenu: [
+        {role: 'about'},
+        {
+          role: 'quit'
+        }]
+    }]
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+  } else {
+    // windows及linux系统
+    Menu.setApplicationMenu(null)
+  }
+}
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -102,7 +103,8 @@ app.on('ready', async () => {
   //     console.error('Vue Devtools failed to install:', e.toString())
   //   }
   // }
-  createWindow()
+  createWindow();
+  startServer(); // 启动服务器
 })
 
 // Exit cleanly on request from parent process in development mode.
@@ -119,3 +121,41 @@ if (isDevelopment) {
     })
   }
 }
+
+// 启动本项目中的服务器
+function startServer() {
+  const cmdStr = 'java -jar 360Hotel-0.0.1-SNAPSHOT.jar' // 要运行的命令
+  // let cmdStr="start cmd"
+  // let serverPath=isDevelopment?"server":"../server" // 注意开发环境和线上环境的路径不同；
+  const serverPath = 'server';
+  runExec(cmdStr);
+  function runExec(cmdStr) {
+    // exec 函数 第一个参数是要执行的命令，第二个函数是配置选项，第三个参数是回调函数，配置项中常用到 子进程的工作目录
+    serverProcess = require('child_process').exec(cmdStr, {cwd: serverPath})
+    serverProcess.stdout.on('data', function(data) {
+      console.log('启动服务器成功 stdout:' + data)// 打印正常的后台可执行程序输出
+    })
+    serverProcess.stderr.on('data', function(data) {
+      console.log('stderr:' + data) // 打印错误的后台可执行程序输出
+    })
+    serverProcess.on('close', function (code) {
+      console.log('out code:' + code) // 退出之后的输出
+    })
+  }
+}
+// 关闭项目中的所有进程，主要是为了关闭刚刚启动的服务器进程。
+function stopServer() {
+  console.log('Kill server process.....');
+  const kill = require('tree-kill'); //  tree-kill是一个插件，需要安装，在项目中已经用 yarn add tree-kill 命令安装。
+  if (serverProcess) {
+    kill(serverProcess.pid, 'SIGTERM', function(err) { // 终止进程树，杀死所有进程，包括根进程。
+      console.log('Killed');
+      console.log(err);
+      serverProcess = null;
+      win = null;
+      app.quit();
+    })
+  }
+}
+
+app.stopServer = stopServer;
