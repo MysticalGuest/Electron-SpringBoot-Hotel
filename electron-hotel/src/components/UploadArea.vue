925a2517892fea802520ee3acf3df65530c00f62
MysticalGuest <1317148109@qq.com>
2020-07-31 11:20:59 +0800

    Create UploadArea.vue

---
+++ electron-hotel/src/components/UploadArea.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-upload
+    class="upload-demo"
+    drag
+    action="https://jsonplaceholder.typicode.com/posts/"
+    multiple
+    v-show="visible">
+    <i class="el-icon-upload"></i>
+    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+    <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+  </el-upload>
+</template>
+
+<script>
+
+export default {
+  props: ['visible'],
+  data() {
+    return {
+      // visible : false
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
