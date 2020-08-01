b617fcf3fe5f4f90cd08610ce5f9ced7c10761dc
MysticalGuest <1317148109@qq.com>
2020-08-01 18:58:46 +0800

    Delete server.js

--- electron-hotel/src/assets/custom/server.js
+++
@@ -1,4 +0,0 @@
-// var cmd = new ActiveXObject('WScript.Shell');
-// var cmdStr = 'cmd /c D: && cd D:/debug/debug && java -jar getBaseInfo.jar';
-// cmd.run(cmdStr);
-// cmd.run('cmd.exe /k cd C:/');
