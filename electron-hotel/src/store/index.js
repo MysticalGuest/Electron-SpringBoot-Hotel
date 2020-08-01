5ba98d0bb309037a341e28011adafbe58686cd5d
MysticalGuest <1317148109@qq.com>
2020-08-01 18:59:29 +0800

    Update index.js

--- electron-hotel/src/store/index.js
+++ electron-hotel/src/store/index.js
@@ -5,8 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: ''
+    token: '',
+    loadState: 0
   },
+  // 同步方法
   mutations: {
     // set方法
     // 修改token，并将token存入localStorage
@@ -20,9 +22,29 @@ export default new Vuex.Store({
       // console.log('jsonaaaaa:  ', json.aName);
       // sessionStorage.setItem('token', user.token);
       sessionStorage.setItem('token', param);
+    },
+    // loadingState (state, load) {
+    //   console.log('loadSTATE:' + load.state);
+    //   this.loadState = load.state;
+    //   console.log('this.loadStateloadSTATE:' + this.loadState);
+    // }
+    loadingState (state) {
+      console.log('BEFORE' + state.loadState);
+      state.loadState = 1;
+      console.log('this.loadStateloadSTATE:' + state.loadState);
     }
   },
+  getters: {
+    getLoading(state) {
+      return state.loadState;
+    }
+  },
+  // 异步方法
   actions: {
+    setloadingState(context) {
+      // 处理异步操作
+      context.commit('loadingState')
+    }
   },
   modules: {
   }
